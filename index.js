42665a0249d178c4fa04ba81117946c1399465fa
phuchv <vanphuc126.bkhn@gmail.com>
2023-04-13 01:31:38 +0700

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    var a = 1;
-    var b = 2;
+app.use(morgan('combined'))
 
-    var c = a + b;
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
